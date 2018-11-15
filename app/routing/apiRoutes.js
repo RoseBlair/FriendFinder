24f0e70abecd8108e6c17bf00f675d51c242b281
Carolyn Bernier <carolynbbernier@gmail.com>
2018-11-15 16:30:57 -0500

    troubleshooting API paths

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,66 +5,66 @@ var path = require("path");
 
 var friends = require("../data/friends.html");
 
-var surveys = require("../public/data/survey.html");
-
-function displayFriends(req, res) {
-    res.json(friends.data);
-}
-
-function newFriend (req, res) {
-    var entry = {
-        name: req.body.namme,
-        photo: req.body.photo,
-        scores: []
-    }
-};
+var newFriend = require("../public/survey.html");
 
 
 
+module.exports = function(app) {
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-    // res.send("is this working?");
-});
+    app.get("/api/friends", function(req, res) {
+        //  res.send(req.params.friends);
+        res.json(friends);
+    });
 
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-});
+    app.post("/api/friends", function(res, req){
+        res.json(newFriend);
+        });
 
- app.get("/api/friends", function(req, res) {
-    //  res.send(req.params.friends);
-     res.json(friends);
- });
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "..", "public", "home.html"));
+        // res.send("is this working?");
+        });
+    
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "..", "data", "survey.html"));
+        });
 
-app.post("/api/friends", function(req, res) {
-    console.log(req.body);
-     var newFriend = {
-        name: "Mary Sanders",
-        photo: "http://static.tvtropes.org/pmwiki/pub/images/AverageMan1.jpg",
-        scores: [
-            2, 
-            3, 
-            2, 
-            3, 
-            2,
-            3,
-            2,
-            3,
-            2,
-            3
-        ]
-    }
 
-    friends.push(newFriend);
-    res.json(friends);
+    };
 
- });
 
- app.post("/api/friends", function(req, res) {
-    console.log(formData);
+
     
 
-    friends.push(formData);
-    res.json(friends);
+    
 
- });
+    app.post("/api/friends", function(req, res, friendRequire) {
+        console.log(req.body);
+        //  var newFriend = {
+        //     name: "Mary Sanders",
+        //     photo: "http://static.tvtropes.org/pmwiki/pub/images/AverageMan1.jpg",
+        //     scores: [
+        //         2, 
+        //         3, 
+        //         2, 
+        //         3, 
+        //         2,
+        //         3,
+        //         2,
+        //         3,
+        //         2,
+        //         3
+        //     ]
+        // }
+
+        friends.push(newFriend);
+        res.json(friends);
+
+    });
+
+
+    app.get("/api/friends", function(req, res) {
+        json.res(newFriend);
+        });
+
+};
