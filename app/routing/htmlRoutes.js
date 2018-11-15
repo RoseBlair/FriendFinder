adb6957f59d3eabbefc7150939e036e862a6d1dd
Carolyn Bernier <carolynbbernier@gmail.com>
2018-11-15 12:40:23 -0500

    updating html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,27 @@
 var path = require("path");
 
-function display (req, res) {
+var htmlRoutes = function (app) {
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "app/public/survey.html"))
+    });
 
-    };
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "app/public/home.html"))
+    })
+}
 
-module.exports = [
-    {url: "/",
-    method: "GET",
-    handler: displayRoot
-},
-    {url: "survey", 
-    method: "GET",
-    handler: displaySurvey}
-]
+module.exports = htmlRoutes;
+
+// function display (req, res) {
+
+//     };
+
+// module.exports = [
+//     {url: "/",
+//     method: "GET",
+//     handler: displayRoot
+// },
+//     {url: "survey", 
+//     method: "GET",
+//     handler: displaySurvey}
+// ]
